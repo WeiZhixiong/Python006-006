672785d0e47027be2669c0334270f982c12661b1
WeiZhixiong <zhixiong.wei@qq.com>
2021-01-26 20:59:49 +0800

    simplified code

--- week04/douban/movie/views.py
+++ week04/douban/movie/views.py
@@ -3,14 +3,11 @@ from .models import Movie, MovieComment
 
 
 def index(request):
-    movie = Movie.objects.order_by('-movie_id')[0]
+    movie = Movie.objects.order_by('-movie_id').first()
     movie_name = movie.movie_name
     movie_id = movie.movie_id
     user_query_dict = request.GET
-    query_str = None
-    if 'comment' in user_query_dict:
-        query_str = user_query_dict["comment"]
-
+    query_str = user_query_dict.get("comment")
     if query_str:
         comment_list = MovieComment.objects.filter(
             movie_id=movie_id,
