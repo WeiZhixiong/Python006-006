f3616c9821f0c5ba9ce1964e6a2c432a301a1fd9
WeiZhixiong <zhixiong.wei@qq.com>
2021-01-26 20:58:19 +0800

    model add __str__ method

--- week04/douban/movie/models.py
+++ week04/douban/movie/models.py
@@ -7,6 +7,9 @@ class Movie(models.Model):
     movie_name = models.CharField(max_length=200, unique=True)
     movie_rating = models.FloatField()
 
+    def __str__(self):
+        return self.movie_name
+
 
 class MovieComment(models.Model):
     comment_id = models.AutoField(primary_key=True)
@@ -14,3 +17,6 @@ class MovieComment(models.Model):
     comment = models.TextField()
     star = models.FloatField()
     create_time = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.comment
