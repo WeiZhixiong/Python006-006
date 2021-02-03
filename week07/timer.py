6490b95dbb80e7bb40c97f723a51b00817bbb1c6
WeiZhixiong <zhixiong.wei@qq.com>
2021-02-03 19:35:22 +0800

    add timer.py

---
+++ week07/timer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import time
+import logging
+from functools import wraps
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def timer(func):
+    """统计程序执行时间"""
+    @wraps(func)
+    def decorate(*args, **kwargs):
+        logger.info(f"start execute function {func.__name__}.")
+        start_time = time.time()
+        func(*args, **kwargs)
+        spend_time = time.time() - start_time
+        logger.info(f"execute function {func.__name__} end; spend time: {spend_time}s")
+    return decorate
+
+
+@timer
+def test(*args, **kwargs):
+    logger.info(f"args: {args}; kwargs: {kwargs}")
+    time.sleep(1)
+
+
+if __name__ == "__main__":
+    test(1, 2, 3, aa=1, bb=2)
